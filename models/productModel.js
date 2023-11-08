b215366ef703ac5404b6c89ba5e3e02d06543980
Ahmed Sayed <therealahme@gmail.com>
2023-11-08 21:08:23 +0200

    create products model

---
+++ models/productModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide the product name.'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please specify the product price.'],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ['ikea', 'liddy', 'caressa', 'marcos'],
+      message: 'Company name must be one of: ikea, liddy, caressa, marcos.',
+    },
+  },
+});
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
